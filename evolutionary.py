956df243c08ff78a589bcb4668753f8c005a52ff
mpirand <cs04436@uoi.gr>
2022-05-09 19:51:10 +0300

    Implemented some routines

--- evolutionary.py
+++ evolutionary.py
@@ -6,7 +6,7 @@ NUM_OF_ITERATIONS = 50
 
 x1 = []
 x2 = []
-F = x1^2 + x2 # the problem function
+F = [] # the problem function
 
 
 # helper routine to generate random values in the range [-1024, 1023]
@@ -18,29 +18,44 @@ def genRandomValues():
 
     return temp
 
-
-def calculate_fitness(F):
-    fitness, total_fitness, mean_fitness = 0
+# calculate the input function and evaluate fitness values for a whole generation
+def evaluate(F):
+    fitness = []
+    total_fitness, mean_fitness = 0
 
     for i in range(POPULATION_SIZE):
-        fitness[i] = x1^2 + x2
+        F[i] = x1[i]^2 + x2[i]
+        fitness[i] = F[i]
+        total_fitness += fitness[i]
 
-
-
-
-
+    mean_fitness = total_fitness / POPULATION_SIZE
     fitness_stats = (fitness, total_fitness, mean_fitness)
     return fitness_stats
 
-def init():
+def select(parents, fitness, total_fitness):
+    
+
+    return offsprings
+
+
+def initialize():
     # initial population
+    global x1, x2
     x1 = genRandomValues()
     x2 = genRandomValues()
 
 def calculate_minimum(F):
     minimum = 0
+    generation = []
 
+    initialize() # create a starting population
 
+    # evaluate the starting population
+    fitness, total_fitness, mean_fitness = evaluate(F) 
+
+    # runs for each evolution step
+    for e in range(NUM_OF_ITERATIONS):
+        
 
 
 
@@ -48,3 +63,8 @@ def calculate_minimum(F):
 
     return minimum
 
+
+
+
+
+calculate_minimum(F) # run simulation
