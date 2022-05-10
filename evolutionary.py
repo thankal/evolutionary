968ad3fd895b3d6f212561ade7bec5b4085b55cb
mpirand <cs04436@uoi.gr>
2022-05-10 19:58:52 +0300

    Major changes and fixes

--- evolutionary.py
+++ evolutionary.py
@@ -8,19 +8,17 @@ NUM_OF_ITERATIONS = 50
 CROSSOVER_PROBABILITY = 0.8
 MUTATION_PROBABILITY = 0.2
 
-x1 = []
-x2 = []
-F = [] # the problem function
-
 r = 0 # the random pool index (keep track of what random value to fetch next)
 
 # helper routine to generate random values in the range [-1024, 1023]
 def initialize():
     # initial population
-    global x1, x2
-    x1 = genRandomValues()
-    x2 = genRandomValues()
+    starting_population = []
 
+    for i in range(POPULATION_SIZE):
+        starting_population[i] = genRandomChromosome() # just a population of random chromosomes (starting values for variables x1, x2)
+
+    return starting_population
 
 # generate a random pool of numbers (20) in range [0, 1] for use later. 
 def generateRandomPool():
@@ -38,33 +36,33 @@ def getNextRandom(random_pool):
 
 
 # generate random values for the initial population
-def genRandomValues():
-    temp = [] # make a list that holds the random values
+def genRandomChromosome():
+    random_chromosome = bin(randint(-1024, 1023))[2:] # random integer in binary representation for variable x1
+    random_chromosome += bin(randint(-1024, 1023))[2:] # same for x2
 
-    for i in range (POPULATION_SIZE):
-        temp[i] = randint(-1024, 1023)
-
-    return temp
+    return random_chromosome
 
 
 # calculate the input function and evaluate fitness values for a whole generation
-def evaluate(F):
+def evaluate(population):
     fitness = []
     total_fitness, mean_fitness = 0
 
     for i in range(POPULATION_SIZE):
-        F[i] = x1[i]^2 + x2[i]
-        fitness[i] = F[i]
+        x1x2 = population[i] # inlcudes x1 bits with x2 bits (total 22 bits)
+        x1 = int(x1x2[11:]) # first 11 bits -> to int
+        x2 = int(x1x2[-11:]) # last 11 bits -> to int
+
+        fitness[i] = x1^2 + x2
         total_fitness += fitness[i]
 
     mean_fitness = total_fitness / POPULATION_SIZE
     fitness_stats = (fitness, total_fitness, mean_fitness)
     return fitness_stats
 
-def select(in_population, fitness, total_fitness):
+def select(in_population, fitness, total_fitness, random_pool):
     selection_probabilities = [] 
     additive_probabilities = [] 
-    random_pool = generateRandomPool()
     out_population = [] # the output of the function
 
     # for every chromosome in the population, calculate their selection probabilities...
@@ -78,13 +76,13 @@ def select(in_population, fitness, total_fitness):
     # now initiate the selection proccess
     for i in range(POPULATION_SIZE):
         for j in range(POPULATION_SIZE):
-            if (getNextRandom() <= additive_probabilities[j]):
+            if (getNextRandom(random_pool) <= additive_probabilities[j]):
                 out_population[i] = in_population[j] 
 
     # return the selected chromosomes
     return  out_population 
 
-def mutate(in_population):
+def mutate(in_population, random_pool):
     out_population = []
     mask = "0b0000000000000000000000" # make a mask in order to change one bit of the chromosome (22 bits)
 
@@ -98,7 +96,7 @@ def mutate(in_population):
             mask += temp
             
         # depending on the mutation probability..
-        if (getNextRandom() < MUTATION_PROBABILITY)
+        if (getNextRandom(random_pool) < MUTATION_PROBABILITY):
             # ..xor that mask with the chromosome so that it gets mutated 
             out_population[i] = in_population[i] ^ mask;
 
@@ -106,55 +104,74 @@ def mutate(in_population):
     return  out_population 
 
 # crossover chromosomes in pairs of two
-def crossover(in_population):
+def crossover(in_population, random_pool):
     out_population = []
 
     # get pairs..
     for i in range(1, POPULATION_SIZE, 2):
         # ..and depending on the crossover probability..
-        if (getNextRandom() < CROSSOVER_PROBABILITY):
+        if (getNextRandom(random_pool) < CROSSOVER_PROBABILITY):
             # ..find the crossover point
-            crossover_point = getNextRandom()*10 # at what digit we take the crossover (e.g. 0.45 -> after 4th digit)
+            crossover_point = getNextRandom(random_pool)*10 # at what digit we take the crossover (e.g. 0.45 -> after 4th digit)
+
+            ## The chromosome includes the concatenation of two variables x1 and x2.
+            ## We need one crossover point so either set the crossover point on x1 or x2
+            ## If we included all the binary digits from the crossover point and after, the crossover would mess up the variable values completely; we do not want that
 
             # only change x1 value
             pad=0
 
             # only change x2 value
-            if getNextRandom() >= 0.5 :
+            if getNextRandom(random_pool) >= 0.5 :
                 pad=11 # a.k.a. begin from the 11th bit
             
 
+            # get the pair to initiate the crossover on
             chromosome1 = in_population[i-1]
             chromosome2 = in_population[i]
 
+            temp = []
             # ..and swap the bits between the chromosomes
-            for int b in range(11):
-                chromosome1[crossover_point]
+            for b in range(11-crossover_point): # only include one variable e.g. xxxxxx[xxxxx]|yyyyyyyyyyy - ([crossover_bits])
+                chromosome1[pad+crossover_point] = temp[b]
+                chromosome1[pad+crossover_point] = chromosome2[pad+crossover_point]
+                chromosome2[pad+crossover_point] = temp[b]
+        
+            # save the changes to the output population
+            out_population[i-1] = chromosome1
+            out_population[i] = chromosome2
 
 
     # return the modified population
     return out_population
 
 
+def printPopulation(population):
+    temp =  ''
+    for c in population:
+        temp += f"{c}\n"
+    return temp
+
+
 # excecutes the evolutionary algorithm
-def calculate_minimum(F):
+def calculate_minimum():
     minimum = 0
-    generation = []
 
 
-    initialize() # create a starting population
+    generation = initialize() # create a starting population
     random_pool = generateRandomPool() # create the random number pool
 
     # evaluate the starting population
-    fitness, total_fitness, mean_fitness = evaluate(F) 
 
     # runs for each evolution step
     for e in range(NUM_OF_ITERATIONS):
-        
+        fitness, total_fitness, mean_fitness = evaluate(generation) 
+        crossover(generation, random_pool)
+        mutate(generation, random_pool)
+        generation = select(generation, fitness, total_fitness, random_pool)
 
-
-
-
+        printPopulation(generation)
+        print('\n')
 
     return minimum
 
